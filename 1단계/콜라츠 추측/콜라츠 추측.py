980b0eb6e1013c4893cfae7cb20399d1e5a2c4b7
HanBeen Lee <40820405+hanbeene@users.noreply.github.com>
2021-02-03 17:37:38 +0900

    Add files via upload

---
+++ 1단계/콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,19 @@
+def solution(num):
+    if num == 1:
+        return 0
+    count = 0
+    while True:
+        if num%2 == 0:
+            num = num//2
+        else:
+            num = (num*3)+1
+        count += 1
+        if num == 1:
+            break
+        if count >= 500:
+            count = -1
+            break
+    return count
+
+n = 1
+print(solution(n))
