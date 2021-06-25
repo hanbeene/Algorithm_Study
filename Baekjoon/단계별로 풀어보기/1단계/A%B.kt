ffeb9a1523132fcc50400130b9c687f96f1fde5d
HanBeen Lee <40820405+hanbeene@users.noreply.github.com>
2021-06-25 09:34:49 +0900

    Add files via upload

---
+++ Baekjoon/단계별로 풀어보기/1단계/A%B.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`);
+    var A = sc.nextDouble();
+    var B = sc.nextDouble();
+
+    println(A/B)
+}
---
+++ Baekjoon/단계별로 풀어보기/1단계/A-B.kt
@@ -0,0 +1,9 @@
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    var sc: Scanner = Scanner(System.`in`);
+    var A = sc.nextInt();
+    var B = sc.nextInt();
+    println(A - B);
+}
